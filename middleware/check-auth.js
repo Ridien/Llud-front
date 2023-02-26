fc842b621b06bb798c30d4887103369e57c550d3
ThomasRidien <thomashopn@gmail.com>
2023-02-26 17:36:39 -0300

    fix:generate

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -8,6 +8,9 @@ export default function ({
     route,
     app
 }) {
+    if(!process.client){
+        return
+    }
     let token = null
     let role = null
     let routeWhiteList = ['index', 'register']
