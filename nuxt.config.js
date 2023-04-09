ffb9aca945caa8e43055e79aa848da1546d27cbd
Ridien <bohemiatech@gmail.com>
2023-04-09 19:32:07 -0300

    fix: sets correct url value

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,7 +73,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://llud-backend.onrender.com',
+    baseURL: 'https://cloudy-galoshes-fish.cyclic.app/',
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
