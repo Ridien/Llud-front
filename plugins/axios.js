3886307ae86e68e0129a360dac753ca410e29dd3
Ridien <bohemiatech@gmail.com>
2023-04-09 22:17:34 -0300

    fix: changes origin

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,4 +1,4 @@
 export default ({ app, req, store, redirect }, {}) => {
-    app.$axios.defaults.baseURL = app.$env['API_URL'] || process.env.API_URL || 'https://llud-backend.onrender.com'
+    app.$axios.defaults.baseURL = app.$env['API_URL'] || process.env.API_URL || 'https://cloudy-galoshes-fish.cyclic.app/'
     app.$axios.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
 }
